79ef3badc6dc2b08ebdf4e10cfa589bac6d066e5
codemkim <inkin61@naver.com>
2022-01-13 16:39:06 +0900

    init

---
+++ movies/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Movie
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'title','genre','year')
